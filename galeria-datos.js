91e740ccda2878f067f4e8ce11c381f7465254d5
stefania818 <stefanitulvm18@gmail.com>
2026-08-26 23:36:29 -0600

    Create galeria-datos.js

---
+++ galeria-datos.js
@@ -0,0 +1,22 @@
+// galeria-datos.js - Repositorio de imágenes para Casa Boxing KO
+
+const GALERIA_IMAGENES = {
+  logo: "https://i.ibb.co/5hjFsYWg/image.png",
+  
+  carrusel: [
+    { src: "https://i.ibb.co/JWf5kcC6/IMG-7789.jpg", alt: "Casa Boxing KO", activa: true },
+    { src: "https://i.ibb.co/pv2y0dpG/IMG-7790.jpg", alt: "Entrenamiento Casa Boxing KO", activa: false },
+    { src: "https://i.ibb.co/TMPyKwGJ/IMG-7786.jpg", alt: "Campeones Casa Boxing KO", activa: false },
+    { src: "https://i.ibb.co/kVz42tB5/IMG-7791.jpg", alt: "Casa Boxing KO", activa: false }
+  ],
+
+  comunidad: [
+    { src: "https://i.ibb.co/pv2y0dpG/IMG-7790.jpg", alt: "Entrenamiento Sparring CBK" }
+  ],
+
+  tienda: {
+    guantes: "https://i.ibb.co/51ad7800-965f-4503-bb4c-329dc83d91d9.jpg",
+    vendas: "https://i.ibb.co/75861512-1a09-45a2-bc52-b07b789c4c07.jpg",
+    bucales: "https://i.ibb.co/fb5f4b4d-af8e-4027-b6af-96cbe2589185.jpg"
+  }
+};
